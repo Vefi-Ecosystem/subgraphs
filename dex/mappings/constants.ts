f476efe05f9f8527d061922c737c9e6bcaf66811
Kingsley Victor <javaprodigy56@gmail.com>
2023-01-07 15:23:20 +0100

    deploy to polygon

--- dex/mappings/constants.ts
+++ dex/mappings/constants.ts
@@ -8,6 +8,6 @@ export const ONE_BD = BigDecimal.fromString("1");
 export const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000";
 export const BI_18 = BigInt.fromI32(18);
 
-export const WETH = "0x15EDEa3D3b4C59E8d76B8BF9374ed4f60F58e3b7";
-export const WETH_USDT_PAIR = "0xa30633b849a147c35f7b5bedf79e2f4899f05d3c";
-export const WETH_USDC_PAIR = "0x6b3f34a8d9f4c7e293397343ff68464e71baef69";
+export const WETH = "0x15edea3d3b4c59e8d76b8bf9374ed4f60f58e3b7";
+export const WETH_USDT_PAIR = "0x5cd8fef362805451550a6fc6cc53c6fdae41e72a";
+export const WETH_USDC_PAIR = "0xba42e0da73d48dad7c40ef040a56eabfea60ed72";
--- dex/mappings/pricing.ts
+++ dex/mappings/pricing.ts
@@ -5,28 +5,30 @@ import { factoryContract } from "./utils";
 
 export const getETHPriceInUSD = (): BigDecimal => {
   const usdtPair = Pair.load(WETH_USDT_PAIR); // usdt is token1;
-  const usdcPair = Pair.load(WETH_USDC_PAIR); // usdc is token0;
+  const usdcPair = Pair.load(WETH_USDC_PAIR); // usdc is token1;
 
   if (!!usdtPair && !!usdcPair) {
     const totalLiquidityETH = usdtPair.reserve0.plus(usdcPair.reserve1);
     if (totalLiquidityETH.notEqual(ZERO_BD)) {
       const usdtWeight = usdtPair.reserve0.div(totalLiquidityETH);
-      const usdcWeight = usdtPair.reserve1.div(totalLiquidityETH);
-      return usdtPair.token1Price.times(usdtWeight).plus(usdcPair.token0Price.times(usdcWeight));
+      const usdcWeight = usdtPair.reserve0.div(totalLiquidityETH);
+      return usdtPair.token1Price.times(usdtWeight).plus(usdcPair.token1Price.times(usdcWeight));
     }
     return ZERO_BD;
   } else if (!!usdtPair) {
     return usdtPair.token1Price;
   } else if (!!usdcPair) {
-    return usdcPair.token0Price;
+    return usdcPair.token1Price;
   }
   return ZERO_BD;
 };
 
 const WHITELIST: Array<string> = [
   WETH,
-  "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174", // USDC
-  "0xc2132D05D31c914a87C6611C10748AEb04B58e8F", // USDT
+  "0x2791bca1f2de4661ed88a30c99a7a9449aa84174", // USDC
+  "0xc2132d05d31c914a87c6611c10748aeb04b58e8f", // USDT
+  "0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6", // WBTC
+  "0x7ceb23fd6bc0add59e62ac25578270cff1b9f619" // WETH (Wrapped Ether)
 ];
 
 const MINIMUM_LIQUIDITY_THRESHOLD_ETH = BigDecimal.fromString("10");
