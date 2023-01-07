91615e5039a926018d1a80d4bb478b1a8792c28f
Kingsley Victor <javaprodigy56@gmail.com>
2023-01-07 14:51:38 +0100

    000

--- dex/mappings/constants.ts
+++ dex/mappings/constants.ts
@@ -8,6 +8,6 @@ export const ONE_BD = BigDecimal.fromString("1");
 export const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000";
 export const BI_18 = BigInt.fromI32(18);
 
-export const WETH = "0xD75411C6A3fEf2278E51EEaa73cdE8352c59eFEd";
+export const WETH = "0xd75411c6a3fef2278e51eeaa73cde8352c59efed";
 export const WETH_USDT_PAIR = "0xa30633b849a147c35f7b5bedf79e2f4899f05d3c";
 export const WETH_USDC_PAIR = "0x6b3f34a8d9f4c7e293397343ff68464e71baef69";
