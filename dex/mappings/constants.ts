438a5495c5a82ae00a4cc72728b249699097370e
Kingsley Victor <javaprodigy56@gmail.com>
2023-01-08 15:47:13 +0100

    deploy to smartchain

--- dex/mappings/constants.ts
+++ dex/mappings/constants.ts
@@ -8,7 +8,7 @@ export const ONE_BD = BigDecimal.fromString("1");
 export const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000";
 export const BI_18 = BigInt.fromI32(18);
 
-export const WETH = "0x61f2ddaa57b328fee381d13d2e0e91c604a43ff7";
+export const WETH = "0x2f856544d28c793f4461ce639709aa8c01d12745";
 export const WETH_USDT_PAIR = "0x8ba96c0d20b673c101a7b0055614be2aa3aa8838";
 export const WETH_USDC_PAIR = "0x55a3c6a554742ca5a0e9f2da5983e194392f4988";
 export const WETH_BUSD_PAIR = "0x5275e891869f1c7be174669fa4574ce9992e7b2f";
